abb15c8ad9bd65441ec7c45e9f234d015d0bdb1f
aalcott14 <aalcott14@gmail.com>
2017-04-07 11:43:42 -0500

    make bold elements on hover

--- app.js
+++ app.js
@@ -1,22 +1,53 @@
 var App = () => (
   <div>
     <h2>Grocery List</h2>
-    <GroceryList />
+    <GroceryList items={['Cucumbers', 'Kale']} />
   </div>
   );
 
-var GroceryList = () => (
-  <ul>
-    <Cucumbers />
-    <Kale />
-  </ul>
-  );
- var Cucumbers = () => (
-  <li>Cucumbers</li>
-  );
- var Kale = () => (
-  <li>Kale</li>
-  );
+class GroceryList extends React.Component {
+  constructor(props) {
+    //React.Component.call(this,props);
+    super(props);
+  }
 
- 
+  render() {
+    return (<ul>
+      {this.props.items.map(item =>
+        <GroceryListItem item={item} />)}
+    </ul>)
+  }
+};
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hover: false
+    }
+  }
+
+  onItemHover() {
+    this.setState({
+      hover: true
+    }); 
+  }
+  onItemUnhover() {
+    this.setState({
+      hover: false
+    })
+  }
+
+  render() {
+    var style = {
+      'font-weight': this.state.hover ? 'bold' : 'normal'
+    }
+    return (
+      <li style = {style} onMouseOver={this.onItemHover.bind(this)} onMouseOut={this.onItemUnhover.bind(this)}>{this.props.item} </li>)
+  }
+
+}
+
+
 ReactDOM.render(<App />, document.getElementById("app"));
