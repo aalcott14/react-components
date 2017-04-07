1853cd9f5886dcd422fe91aba0c9ca7d867cc756
aalcott14 <aalcott14@gmail.com>
2017-04-07 10:43:42 -0500

    first part of tutorial

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// TODO
+var App = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList />
+  </div>
+  );
+
+var GroceryList = () => (
+  <ul>
+    <Cucumbers />
+    <Kale />
+  </ul>
+  );
+ var Cucumbers = () => (
+  <li>Cucumbers</li>
+  );
+ var Kale = () => (
+  <li>Kale</li>
+  );
+
+ 
+ReactDOM.render(<App />, document.getElementById("app"));
